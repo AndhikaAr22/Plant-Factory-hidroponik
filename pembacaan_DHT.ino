5735b416a69b2d4391b307d32771395037274974
rizkydenianto <rizkydenianto0@gmail.com>
2022-11-02 14:01:52 +0700

    Ngilangin komen di serial.print

--- pembacaan_DHT.ino
+++ pembacaan_DHT.ino
@@ -2,12 +2,12 @@ void pembacaan_DHT () {
 suhu_DHT11 = dht.readTemperature();
 kelembapan_DHT11 = dht.readHumidity();
 
-// Serial.print("Kelembapan : ");
-// Serial.print(kelembapan_DHT11);
-// Serial.println(" %\t");
-//
-// Serial.print("Suhu : ");
-// Serial.print(suhu_DHT11);
-// Serial.println(" C");
+Serial.print("Kelembapan : ");
+Serial.print(kelembapan_DHT11);
+Serial.println(" %\t");
+
+Serial.print("Suhu : ");
+Serial.print(suhu_DHT11);
+Serial.println(" C");
 
  }
